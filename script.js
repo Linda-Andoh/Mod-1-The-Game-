1a4c1d5109464477108c2b95db867cf972c16c32
Linda <lexicon736@gmail.com>
2022-08-31 23:13:44 -0400

    The Game Completed

---
+++ script.js
@@ -0,0 +1,122 @@
+// A SIMPLE TIC-TAC-TOE GAME IN JAVASCRIPT
+// (1) Grid layout
+
+// The game grid is represented in the array Grid.cells as follows:
+
+// [0] [1] [2]
+// [3] [4] [5]
+// [6] [7] [8]
+
+// The cells (array elements) hold the following numeric values:
+// 0 if not occupied, 1 for player, 3 for computer.
+// This allows us to quickly get an overview of the game state:
+// if the sum of all the cells in a row is 9, the computer wins,
+// if it is 3 and all the cells are occupied, the human player wins,
+// etc.
+
+// (2) Strategy of makeComputerMove()
+
+// The computer first  looks for almost completed rows, columns, and
+// diagonals, where there are two fields occupied either by the human
+// player or by the computer itself. If the computer can win by
+// completing a sequence, it does so; if it can block the player from
+// winning with the next move, it does that. If none of that applies,
+// it plays the center field if that's free, otherwise it selects a
+// random free field. This is not a 100 % certain strategy, but the
+// gameplay experience is fairly decent.
+
+// 
+
+
+const X_CLASS = 'x'
+const CIRCLE_CLASS = 'circle'
+const WINNING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+const cellElements = document.querySelectorAll('[data-cell]')
+const board = document.getElementById('board')
+const winningMessageElement = document.getElementById('winningMessage')
+const restartButton = document.getElementById('restartButton')
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
+let circleTurn
+
+startGame()
+
+restartButton.addEventListener('click', startGame)
+
+function startGame() {
+  circleTurn = false
+  cellElements.forEach(cell => {
+    cell.classList.remove(X_CLASS)
+    cell.classList.remove(CIRCLE_CLASS)
+    cell.removeEventListener('click', handleClick)
+    cell.addEventListener('click', handleClick, { once: true })
+  })
+  setBoardHoverClass()
+  winningMessageElement.classList.remove('show')
+}
+
+function handleClick(e) {
+  const cell = e.target
+  const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+  placeMark(cell, currentClass)
+  if (checkWin(currentClass)) {
+    endGame(false)
+  } else if (isDraw()) {
+    endGame(true)
+  } else {
+    swapTurns()
+    setBoardHoverClass()
+  }
+}
+
+function endGame(draw) {
+  if (draw) {
+    winningMessageTextElement.innerText = 'Draw!'
+  } else {
+    winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
+  }
+  winningMessageElement.classList.add('show')
+}
+
+function isDraw() {
+  return [...cellElements].every(cell => {
+    return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+  })
+}
+
+function placeMark(cell, currentClass) {
+  cell.classList.add(currentClass)
+}
+
+function swapTurns() {
+  circleTurn = !circleTurn
+}
+
+function setBoardHoverClass() {
+  board.classList.remove(X_CLASS)
+  board.classList.remove(CIRCLE_CLASS)
+  if (circleTurn) {
+    board.classList.add(CIRCLE_CLASS)
+  } else {
+    board.classList.add(X_CLASS)
+  }
+}
+
+function checkWin(currentClass) {
+  return WINNING_COMBINATIONS.some(combination => {
+    return combination.every(index => {
+      return cellElements[index].classList.contains(currentClass)
+    })
+  })
+}
+
+
+
